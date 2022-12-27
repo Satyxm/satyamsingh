9985c6b882bd780f86fef3ae1048e70201ba2a2e
Satyam Singh <97077594+Satyxm@users.noreply.github.com>
2022-12-27 21:00:25 +0530

    Create sumprod.py
    This program focuses on the basic problem: If the product of the two integer numbers is less than or equal to 2000, then Print the product else print their sum.

---
+++ sumprod.py
@@ -0,0 +1,10 @@
+num1 = int(input("Enter number 1: "))
+num2 = int(input("Enter number 2: "))
+
+prod = num1 * num2
+sum = num1 + num2
+
+if prod <= 2000 :
+    print("Product is: ", prod)
+else :
+    print("Sum is: ", sum)
